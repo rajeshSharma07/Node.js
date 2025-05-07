cfdcdddf5d1fb53b848f843bfe3f842367b8fc1e
Rajesh Sharma <sharmarajesh42225@gmail.com>
2025-05-07 14:17:53 +0530

    node js series

---
+++ math.js
@@ -0,0 +1,12 @@
+const sum =(a,b)=>a+b;
+const nul=(a,b)=>a*b;
+const g= 9.8;
+const PI=3.14;
+
+let obj={
+     sum:sum,
+     nul:nul,
+     g:g,
+     PI:PI,
+};
+module.exports=obj;
---
+++ script.js
@@ -0,0 +1,29 @@
+// let n=5;
+
+// for(let i=0; i<n; i++){
+//     console.log('hello',i);
+
+// }
+
+
+// console.log(process.argv);
+// let arr=process.argv;
+
+// for(let i=2;i<arr.length;i++){
+//     console.log("hello welcome to",arr[i]);
+// }
+
+
+
+// const math=require("./math");
+
+// // console.log(math);
+
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+
+
+
+const info=require("./fruits");
+
+console.log(info);
